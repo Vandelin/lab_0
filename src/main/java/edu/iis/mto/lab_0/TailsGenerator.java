3a0524bb578bccebd239b4abea8c9e3e0d82cd35
Piotr.Rogowski <209446@edu.p.lodz.pl>
2019-03-06 18:44:48 +0100

    Fixed style another push.

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,14 +1,21 @@
 package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
+
         ArrayList<String> temp = new ArrayList<>(value.length() + 1);
-        if(value==null) { return new ArrayList<>(); }
-        for(int i = 0; i<value.length() + 1; i++){ temp.add(value.substring(i)); }
+
+        if (value == null) {
+            return Collections.EMPTY_LIST;
+        }
+        for (int i = 0; i < value.length() + 1; i++) {
+            temp.add(value.substring(i));
+        }
         return temp;
     }
 
